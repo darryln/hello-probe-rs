74c0c0510a6573bacc9f6e805c1e8b55bca141d2
Darryl N <me@example.com>
2022-01-15 21:03:43 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_probe as _;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello, world!");
+
+    for x in 1..=5 { 
+        rprintln!("{}", x)
+    }
+
+    rprintln!("finished!");
+
+    //asm::bkpt()
+    asm::udf();
+}

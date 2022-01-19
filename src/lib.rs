6ada96e1a92d54e8f16e20c3ca08896479cfe449
Darryl N <darryln@example.com>
2022-01-18 21:00:11 -0500

    use hal local repo

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,8 @@ fn panic() -> ! {
 /// Terminates the application and makes `probe-run` exit with exit-code = 0
 pub fn exit() -> ! {
     loop {
-        cortex_m::asm::bkpt();
-        //cortex_m::asm::nop();
+        //cortex_m::asm::bkpt();
+        cortex_m::asm::nop();
     }
 }
 
